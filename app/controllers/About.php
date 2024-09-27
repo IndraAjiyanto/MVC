fe62fb383ce5d0913f263de93dfe08bcbd3e9d45
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-09-28 03:54:54 +0700

    mengatur url untuk mengarah ke direktori controller dan dijalankan

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+class About{
+    public function index($nama='indra', $pekerjaan='web developer'){
+        echo "Perkenalkan nama saya $nama dan pekerjaan saya adalah $pekerjaan";
+    }
+
+    public function page(){
+        echo "About/page";
+    }
+}
