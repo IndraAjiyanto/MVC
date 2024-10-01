59ddde43d8184909eaa07b44f92a94ea0ab385ed
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-10-01 15:17:54 +0700

    menambahkan asset

--- app/view/template/footer.php
+++ app/view/template/footer.php
@@ -1,3 +1,4 @@
+</div>
 <script src="<?php echo BASEURL; ?>/js/bootstrap.js"></script>
 </body>
 </html>
--- app/view/template/header.php
+++ app/view/template/header.php
@@ -7,4 +7,26 @@
     <title><?php echo $data['judul']?></title>
 </head>
 <body>
+    <div class="container">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MVC</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo BASEURL; ?>/Home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo BASEURL; ?>/About">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+
+    <div class="container">
     
