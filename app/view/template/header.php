dae39d216a4ca5e1c7e5acc520e93471dcaa05e5
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-10-01 15:11:55 +0700

    menambahkan asset

--- app/view/template/header.php
+++ app/view/template/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo BASEURL; ?>/css/bootstrap.css">
     <title><?php echo $data['judul']?></title>
 </head>
 <body>
